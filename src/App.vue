cc93dd517e8ebe95227452a282ede1275550f599
Jake <hiba1027@gmail.com>
2020-05-29 00:57:50 +0900

    doit vue.js 실습

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style lang="css">
+  body{
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input{
+    border-style: groove;
+  }
+  .shadow{
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.03)
+  }
+</style>
